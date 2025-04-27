80477cee4a5b7ded0ac17bb4cbbeb56d8c4a3e65
FarmanCoder003 <codingfarman01@gmail.com>
2025-04-27 19:05:07 +0600

    initialize server setup with Express and environment variables

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import e from "express";
+const app = e();
+app.use(e.json({ limit: "1mb" }));
+app.use(e.urlencoded({ limit: "1mb", extended: true }));
+
+app.get("/", (_, res) => {
+  res.send("server is running");
+});
+app.use((_, res) => {
+  res.send("404 not found");
+});
+export { app };
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const PORT = process.env.PORT;
+const APP_URL = process.env.APP_URL;
+
+export { APP_URL, PORT };
