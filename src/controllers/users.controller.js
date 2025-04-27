6ca95f783457babf2077e7da3560ebbc1527ef6a
FarmanCoder003 <codingfarman01@gmail.com>
2025-04-27 21:22:39 +0600

    feat: implement user creation with validation and password hashing

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/users.schema.js";
+const createUser = async (req, res) => {
+  try {
+    const { displayname, email, password } = req.body;
+    if ([displayname, email, password].some((value) => !value)) {
+      return res.status.json({ message: "All fields are required" });
+    }
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      displayname,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {}
+};
